a63ce9aa7f09227ea35b6af92f3e24ad42fef7ec
alexandrapesantez <103144953+alexandrapesantez@users.noreply.github.com>
2024-04-09 17:15:43 -0600

    added cookies to program and password stuff to program.cs

--- LegoSolution1-10/Program.cs
+++ LegoSolution1-10/Program.cs
@@ -16,6 +16,17 @@ builder.Services.AddDbContext<LegoDatabaseContext>(options =>
     options.UseSqlite(builder.Configuration["ConnectionStrings=legoConnection"]);
 });
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Default Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 14;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
     options.CheckConsentNeeded = context => true;
