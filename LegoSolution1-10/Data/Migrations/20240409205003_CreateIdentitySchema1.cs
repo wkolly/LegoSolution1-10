187f5529e455348cd75c0a499b4b2b8fc1db3c8d
justinchewsta <justinchewsta@gmail.com>
2024-04-09 14:57:29 -0600

    multi factor

---
+++ LegoSolution1-10/Data/Migrations/20240409205003_CreateIdentitySchema1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LegoSolution1_10.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIdentitySchema1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
