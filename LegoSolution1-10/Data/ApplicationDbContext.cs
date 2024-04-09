473d5540491dedb3a7217a766616ab5bff47ac3a
William Koelliker <willkoelliker22@gmail.com>
2024-04-09 14:18:21 -0600

    Initial commit

---
+++ LegoSolution1-10/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LegoSolution1_10.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
---
+++ LegoSolution1-10/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LegoSolution1_10.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
