aa2a5b9620b9a26a9bab558cd33c0a4cd76b1bf9
ryantetro <Ryantetro@gmail.com>
2024-04-10 12:05:20 -0600

    CRUD Admin

--- LegoSolution1-10/Models/EFLegoRepository.cs
+++ LegoSolution1-10/Models/EFLegoRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace LegoSolution1_10.Models;
 
     public class EFLegoRepository : ILegoRepository
@@ -10,4 +12,36 @@ namespace LegoSolution1_10.Models;
         }
 
         public IQueryable<Product> Products => _context.Products;
+        
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product GetProductById(string productId)
+        {
+            return _context.Products.FirstOrDefault(p => p.ProductId == productId);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteProduct(string productId)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == productId);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+        }
     }
