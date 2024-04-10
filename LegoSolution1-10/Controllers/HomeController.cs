82c24774be57daaea9661402f5b9c782654df98f
William Koelliker <willkoelliker22@gmail.com>
2024-04-09 19:19:18 -0600

    more views

--- LegoSolution1-10/Controllers/HomeController.cs
+++ LegoSolution1-10/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ public class HomeController : Controller
         return View(productData);
     }
 
+    public IActionResult ProductDetails()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
